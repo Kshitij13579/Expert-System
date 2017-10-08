e1bd47ccf0c2254b7a17a3c62ccdbff8dc257994
Kshitij13579 <kshitijyerande@gmail.com>
2017-10-08 16:51:21 +0530

    First Commit

---
+++ src/System/MainUI.java
@@ -0,0 +1,67 @@
+package System;
+
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class MainUI extends JFrame {
+	public MainUI(){
+		JFrame f=new JFrame("Traffic Congestion Control");
+		f.setLocation(350, 100);
+		f.setSize(500, 600);
+		f.setLayout(null);
+		
+		JLabel title=new JLabel("Traffic Congestion Control");
+		Font ftitle=new Font("Serif", Font.BOLD, 20);
+		title.setFont(ftitle);
+		title.setBounds(130,15, 300, 30);
+		
+		JLabel label1=new JLabel("Road Size:");
+		Font flabel1=new Font("Serif", Font.BOLD, 15);
+		label1.setFont(flabel1);
+		label1.setBounds(70,100, 100, 30);
+		
+		JLabel label2=new JLabel("Number Of Vehicles:");
+		Font flabel2=new Font("Serif", Font.BOLD, 15);
+		label2.setFont(flabel2);
+		label2.setBounds(70,130, 200, 30);
+		
+		JLabel label3=new JLabel("Busy Hour:");
+		Font flabel3=new Font("Serif", Font.BOLD, 15);
+		label3.setFont(flabel3);
+		label3.setBounds(70,160, 100, 30);
+		
+		JTextField tlabel1=new JTextField(100);
+		tlabel1.setBounds(150, 105, 100, 20);
+		
+		JTextField tlabel2=new JTextField(100);
+		tlabel2.setBounds(220, 135, 100, 20);
+		
+		JComboBox<String> tlabel3=new JComboBox<>();
+		tlabel3.setBounds(150, 165, 100, 20);
+		tlabel3.addItem("Yes");
+		tlabel3.addItem("No");
+		
+		
+		f.add(title);
+		f.add(label1);
+		f.add(label2);
+		f.add(label3);
+		f.add(tlabel1);
+		f.add(tlabel2);
+		f.add(tlabel3);
+		
+		
+		f.setVisible(true);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MainUI();
+	}
+
+}
