fec16cda3866be0273f7e9a2d01fa99a25e499d5
Kshitij13579 <kshitijyerande@gmail.com>
2017-10-11 00:44:30 +0530

    Listener added for input transformation

--- src/System/MainUI.java
+++ src/System/MainUI.java
@@ -2,15 +2,26 @@ package System;
 
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class MainUI extends JFrame {
+public class MainUI extends JFrame implements ActionListener{
+	Input i;
+	public JTextField tlabel1;
+	public JTextField tlabel2;
+	public JComboBox<String> tlabel3;
+	JButton enter;
+	JButton blabel1;
+	JButton blabel2;
+	JButton blabel3;
 	public MainUI(){
 		JFrame f=new JFrame("Traffic Congestion Control");
 		f.setLocation(350, 100);
@@ -37,27 +48,27 @@ public class MainUI extends JFrame {
 		label3.setFont(flabel3);
 		label3.setBounds(70,160, 100, 30);
 		
-		JTextField tlabel1=new JTextField(100);
+		tlabel1=new JTextField(100);
 		tlabel1.setBounds(150, 105, 100, 20);
 		
-		JTextField tlabel2=new JTextField(100);
+		tlabel2=new JTextField(100);
 		tlabel2.setBounds(220, 135, 100, 20);
 		
-		JComboBox<String> tlabel3=new JComboBox<>();
+		tlabel3=new JComboBox<>();
 		tlabel3.setBounds(150, 165, 100, 20);
 		tlabel3.addItem("Yes");
 		tlabel3.addItem("No");
 		
-		JButton enter=new JButton("Enter");
+		enter=new JButton("Enter");
 		enter.setBounds(250, 450, 80, 30);
 		
-		JButton blabel1=new JButton("Input Transformation");
-		blabel1.setBounds(50,500, 130, 30);
+		blabel1=new JButton("Input Transformation");
+		blabel1.setBounds(30,500, 170, 30);
 		
-		JButton blabel2=new JButton("Rule Activation");
+		blabel2=new JButton("Rule Activation");
 		blabel2.setBounds(220, 500, 130, 30);
 		
-		JButton blabel3=new JButton("Belief Update");
+		blabel3=new JButton("Belief Update");
 		blabel3.setBounds(380, 500, 130, 30);
 		
 		f.add(title);
@@ -72,12 +83,25 @@ public class MainUI extends JFrame {
 		f.add(blabel2);
 		f.add(blabel3);
 		
+		blabel1.addActionListener(this);
+		enter.addActionListener(this);
 		
 		f.setVisible(true);
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new MainUI();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==enter){
+			i=new Input(Integer.parseInt(tlabel1.getText()),Integer.parseInt(tlabel2.getText()),tlabel3.getSelectedItem().toString());
+			JOptionPane.showMessageDialog(null, "Data Inserted Succesfully");
+		}
+		if(e.getSource()==blabel1){
+			new TransformationUI(i);
+		}
 	}
 
 }
